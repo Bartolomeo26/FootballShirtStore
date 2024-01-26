45b9598a305c4bfee451c83509cf6c1ad4c451c2
Piotr Mazur <piotrjm02@gmail.com>
2024-01-26 16:19:21 +0100

    .

---
+++ FootballShirtStore/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FootballShirtStore.Entities
+{
+    public class Category
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string CategoryName { get; set; }
+        public Category? ParentCategory { get; set; }
+        public int? ParentCategoryId { get; set; }
+
+        public ICollection<Category> ChildCategories { get; set; } = new List<Category>();
+
+    }
+}
---
+++ FootballShirtStore/Entities/Item.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace FootballShirtStore.Entities
+{
+    public class Item
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public double PriceMultiplier { get; set; } = 1;
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        [Required]
+        public Color Color { get; set; } 
+        [Required]
+        public ItemType Type { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [DeleteBehavior(DeleteBehavior.Restrict)]
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        [DeleteBehavior(DeleteBehavior.Cascade)]
+        public ICollection<FileData> FileData { get; set; } = new List<FileData>();
+    }
+    public enum ItemType
+    {
+        Men,
+        Women,
+        Kids
+    }
+
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Black,
+        White,
+        Gray,
+        Brown,
+        Pink,
+        Purple,
+        Orange
+    }
+}
