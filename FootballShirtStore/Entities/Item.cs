3bfb65c0255871caff3fffd33e917310f6f3cf63
Bartosz Spiżarny <83046@student.pb.edu.pl>
2024-01-26 18:11:31 +0100

    Cos tam

--- FootballShirtStore/Entities/Item.cs
+++ FootballShirtStore/Entities/Item.cs
@@ -10,8 +10,8 @@ namespace FootballShirtStore.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
-        //public string Name { get; set; }
-        //[Required]
+        public string Name { get; set; }
+        [Required]
         public double Price { get; set; }
         [Required]
         public double PriceMultiplier { get; set; } = 1;
