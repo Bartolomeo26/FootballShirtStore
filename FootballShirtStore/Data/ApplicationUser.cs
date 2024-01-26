97e773eba436de3f8c86f1f03040db23241f8a78
Piotr Mazur <piotrjm02@gmail.com>
2024-01-26 16:51:16 +0100

    dd

--- FootballShirtStore/Data/ApplicationUser.cs
+++ FootballShirtStore/Data/ApplicationUser.cs
@@ -1,10 +1,28 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FootballShirtStore.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id {  get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [EmailAddress]
+        [Required]
+        public string EmailAddress { get; set;}
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Address { get; set; }
+        
+
     }
 
 }
