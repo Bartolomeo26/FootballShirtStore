148a3d7cb51963e754db1479359b6c0221bc69f5
Piotr Mazur <piotrjm02@gmail.com>
2024-01-26 17:11:46 +0100

    ddd

--- FootballShirtStore/Data/ApplicationDbContext.cs
+++ FootballShirtStore/Data/ApplicationDbContext.cs
@@ -1,9 +1,16 @@
+using FootballShirtStore.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.IO.Pipelines;
 
 namespace FootballShirtStore.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<FileData> Files { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
     }
 }
