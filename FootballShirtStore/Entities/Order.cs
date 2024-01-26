56c57051835d42e3453d375ac3e8a12a20be4de6
Piotr Mazur <piotrjm02@gmail.com>
2024-01-26 16:26:34 +0100

    .

---
+++ FootballShirtStore/Entities/Order.cs
@@ -0,0 +1,27 @@
+﻿using FootballShirtStore.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FootballShirtStore.Entities
+{
+    public enum State
+    {
+        Accepted,
+        Ongoing,
+        Completed,
+        Cancelled
+    }
+    public class Order
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public double TotalPrice { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public DateTime? DateCompleted { get; set; }
+        public ApplicationUser User { get; set; }
+        public int UserId { get; set; }
+        public State State { get; set; } = State.Accepted;
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
