fdd5c1eab5da6376aefd96b291a36ced5b9f65ca
pawel <pawel@DESKTOP>
2024-01-28 17:43:01 +0100

    Add biedacki visit counter.

--- FootballShirtStore/Data/ApplicationDbContext.cs
+++ FootballShirtStore/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace FootballShirtStore.Data
         public DbSet<Item> Items { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<VisitsCounter> Visits { get; set; }
 
     }
 }
---
+++ FootballShirtStore/Entities/VisitsCounter.cs
@@ -0,0 +1,6 @@
+﻿namespace FootballShirtStore.Entities {
+    public class VisitsCounter {
+        public int Id { get; set; }
+        public int Counter { get; set; }
+    }
+}
